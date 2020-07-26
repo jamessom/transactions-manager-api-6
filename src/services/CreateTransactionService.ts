e7a3f861edfb3bec4d86722c4343e14777febfd7
Jamessom Queiroz <jamessomqueiroz@gmail.com>
2020-07-26 19:06:58 -0300

    Create transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,33 @@
 // import AppError from '../errors/AppError';
-
+import { getCustomRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    type,
+    value,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = transactionsRepository.create({
+      type,
+      value,
+      title,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
